6f1d6168104e9a33cbcf7f82c776a0e7a1256b94
melihemin <melihemin10@gmail.com>
2025-09-19 03:05:33 +0300

    Task 3: Connected lessons, lesson-detail pages to database.

--- router/platform.py
+++ router/platform.py
@@ -5,6 +5,7 @@ from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 from database.settings import SessionLocal
 from typing import Annotated
+from database.models import Lesson, User, User_statistics
 
 router = APIRouter(prefix="/education", tags=["Education Platform"])
 
@@ -25,13 +26,17 @@ def connect():
 db_dependency = Annotated[Session, Depends(connect)]
 
 @router.get("/lessons", summary="Education Platform Home Page")
-async def get_lessons(request: Request):
-    
-    return templates.TemplateResponse("lessons.html", {"request": request})
+async def get_lessons(request: Request, db: db_dependency):
+    lessons = db.query(Lesson).all()
+    print([lesson.title for lesson in lessons] if lessons else "No lessons found")
+    return templates.TemplateResponse("lessons.html", {"request": request, "lessons": lessons})
 
 @router.get("/lessons/{lesson_id}", summary="Education Platform Lesson Detail Page")
-async def get_lesson_detail(request: Request, lesson_id: int):
-    return templates.TemplateResponse("lesson_detail.html", {"request": request, "lesson_id": lesson_id})
+async def get_lesson_detail(request: Request, lesson_id: int, db: db_dependency):
+    lesson = db.query(Lesson).filter(Lesson.id == lesson_id).first()
+    if not lesson:
+        return JSONResponse(status_code=404, content={"message": "Lesson not found"})
+    return templates.TemplateResponse("lesson_detail.html", {"request": request, "lesson": lesson})
 
 @router.get("/lessons/{lesson_id}/unit/{unit_id}", summary="Education Platform Unit Lesson Detail Page")
 async def get_unit_lesson_detail(request: Request, unit_id: int, lesson_id: int):
